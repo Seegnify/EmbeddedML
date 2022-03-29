0af99c23d39f6b7f8864bf88612ffffc2b0d8675
GrootWoods <9101403+GrootWoods@users.noreply.github.com>
2022-03-28 22:05:17 -0400

    Non-Qt Image implementation with crop and scale

---
+++ main/image.cc
@@ -0,0 +1,212 @@
+#include "image.hh"
+
+#include <sstream>
+#include <cstring>
+
+#include <Magick++.h>
+
+namespace seegnify {
+
+void Image::load(const std::string& path)
+{
+  clear();
+
+  Magick::Image image;
+  image.read(path);
+
+  _rows = image.rows();
+  _cols = image.columns();
+  _bits_per_pixel = image.depth();
+
+  Magick::Blob blob;
+
+  switch(_bits_per_pixel)
+  {
+    case 8:
+    {
+      image.magick("R");
+      image.type(Magick::GrayscaleType);
+      image.write(&blob);
+    }
+    case 16:
+    {
+      image.magick("R");
+      image.type(Magick::GrayscaleType);
+      image.depth(16);
+      image.write(&blob);
+    }
+    case 24:
+    {
+      image.magick("RGB");
+      image.write(&blob);
+    }
+    case 32:
+    {
+      image.magick("RGBA");
+      image.write(&blob);
+    }
+  };
+
+  _data = new uint8_t[blob.length()];
+  std::copy_n((uint8_t*)blob.data(), blob.length(), (uint8_t*)_data);
+}
+
+void Image::save(const std::string& path)
+{
+  switch(_bits_per_pixel)
+  {
+    case 8:
+    {
+      Magick::Image image(_cols, _rows, "R", Magick::CharPixel, _data);
+      image.type(Magick::GrayscaleType);
+      image.write(path);
+      break;
+    }
+    case 16:
+    {
+      Magick::Image image(_cols, _rows, "R", Magick::CharPixel, _data);
+      image.type(Magick::GrayscaleType);
+      image.depth(16);
+      image.write(path);
+      break;
+    }
+    case 24:
+    {
+      Magick::Image image(_cols, _rows, "RGB", Magick::CharPixel, _data);
+      image.write(path);
+      break;
+    }
+    case 32:
+    {
+      Magick::Image image(_cols, _rows, "RGBA", Magick::CharPixel, _data);
+      image.write(path);
+      break;
+    }
+    default:
+    {
+      std::ostringstream msg;
+      msg << "Unsupported image depth " << _bits_per_pixel;
+      throw std::runtime_error(msg.str());
+    }
+  };
+}
+
+Image Image::crop(int row, int col, uint32_t rows, uint32_t cols)
+{
+  Image im(rows,  cols, _bits_per_pixel);
+
+  auto bytes_per_pixel = _bits_per_pixel / 8;
+
+  std::memset(im._data, 0, rows * cols * bytes_per_pixel);
+
+  for (int r=0; r<rows; r++)
+  {
+    for (int c=0; c<cols; c++)
+    {
+      auto this_r = r + row;
+      auto this_c = c + col;
+
+      if (this_r >= 0 and this_r < _rows)
+      if (this_c >= 0 and this_c < _cols)
+      {
+        // compute array offset given row order
+        auto offset = bytes_per_pixel * (r * cols + c);
+        auto this_offset = bytes_per_pixel * (this_r * _cols + this_c);
+
+        // copy pixel bytes
+        for (int b=0; b<bytes_per_pixel; b++)
+        {
+          im._data[offset + b] = _data[this_offset + b];
+        }
+      }
+    }
+  }
+
+  return im;
+}
+
+Image Image::scale(uint32_t rows, uint32_t cols,
+Interpolation interp)
+{
+  switch(interp)
+  {
+    case ITERPOLATE_NEAREST:
+      return scale_nearest(rows, cols);
+    case ITERPOLATE_BILINEAR:
+      return scale_bilinear(rows, cols);
+  }
+}
+
+Image Image::scale_nearest(uint32_t rows, uint32_t cols)
+{
+  Image im(rows,  cols, _bits_per_pixel);
+
+  auto scale_r = (float)_rows/rows;
+  auto scale_c = (float)_cols/cols;
+  auto bytes_per_pixel = _bits_per_pixel / 8;
+
+  for (auto r=0; r<rows; r++)
+  for (auto c=0; c<cols; c++)
+  {
+    uint32_t this_c = scale_c * c;
+    uint32_t this_r = scale_r * r;
+
+    auto offset = bytes_per_pixel * (r * cols + c);
+    auto this_offset = bytes_per_pixel * (this_r * _cols + this_c);
+
+    // copy pixel bytes
+    for (int b=0; b<bytes_per_pixel; b++)
+    {
+      im._data[offset + b] = _data[this_offset + b];
+    }
+  }
+
+  return im;
+}
+
+Image Image::scale_bilinear(uint32_t rows, uint32_t cols)
+{
+  Image im(rows,  cols, _bits_per_pixel);
+
+  auto scale_r = (float)_rows/rows;
+  auto scale_c = (float)_cols/cols;
+  auto bytes_per_pixel = _bits_per_pixel / 8;
+
+  for (auto r=0; r<rows; r++)
+  for (auto c=0; c<cols; c++)
+  {
+    auto float_r = scale_r * r;
+    auto float_c = scale_c * c;
+
+    uint32_t this_r0 = float_r;
+    uint32_t this_c0 = float_c;
+
+    float lr = float_r - this_r0;
+    float lc = float_c - this_c0;
+
+    uint32_t this_r1 = std::min<uint32_t>(this_r0 + 1, _rows-1); 
+    uint32_t this_c1 = std::min<uint32_t>(this_c0 + 1, _cols-1); 
+
+    auto r0_c0 = bytes_per_pixel * (this_r0 * _cols + this_c0);
+    auto r1_c0 = bytes_per_pixel * (this_r1 * _cols + this_c0);
+    auto r0_c1 = bytes_per_pixel * (this_r0 * _cols + this_c1);
+    auto r1_c1 = bytes_per_pixel * (this_r1 * _cols + this_c1);
+
+    auto offset = bytes_per_pixel * (r * cols + c);
+
+    // copy pixel bytes
+    for (int b=0; b<bytes_per_pixel; b++)
+    {
+      // linear along rows
+      auto col_r0 = (1 - lr) * _data[r0_c0 + b] + lr * _data[r1_c0 + b];
+      auto col_r1 = (1 - lr) * _data[r0_c1 + b] + lr * _data[r1_c1 + b];
+
+      // linear along cols
+      im._data[offset + b] = (1 - lc) * col_r0 + lc * col_r1;
+    }
+  }
+
+  return im;
+}
+
+} /* namespace */
---
+++ main/image.hh
@@ -0,0 +1,91 @@
+#ifndef _SEEGNIFY_IMAGE_H_
+#define _SEEGNIFY_IMAGE_H_
+
+#include <string>
+
+namespace seegnify {
+
+class Image
+{
+public:
+
+  enum Interpolation
+  {
+    ITERPOLATE_NEAREST = 1,
+    ITERPOLATE_BILINEAR = 2
+  };
+
+  Image()
+  {
+    _data = nullptr;
+    _rows = 0;
+    _cols = 0;
+    _bits_per_pixel = 0;
+  }
+
+  Image(uint32_t rows, uint32_t cols, uint8_t bits_per_pixel = 24)
+  {
+    _rows = rows;
+    _cols = cols;
+    _bits_per_pixel = bits_per_pixel;
+    _data = new uint8_t[rows * cols * bits_per_pixel / 8];
+  }
+
+  Image(Image&& other)
+  {
+    _rows = other._rows;
+    _cols = other._cols;
+    _bits_per_pixel = other._bits_per_pixel;
+    _data = other._data;
+
+    other._data = nullptr;
+    other._rows = 0;
+    other._cols = 0;
+    other._bits_per_pixel = 0;
+  }
+
+  ~Image()
+  {
+    clear();
+  }
+
+  void clear()
+  {
+    delete [] _data;
+    _data = nullptr;
+    _rows = 0;
+    _cols = 0;
+    _bits_per_pixel = 0;
+  }
+
+  uint8_t *data() { return _data; }
+
+  int32_t rows() { return _rows; }
+
+  int32_t cols() { return _cols; }
+
+  uint8_t bits_per_pixel() { return _bits_per_pixel; }  
+
+  void load(const std::string& path);
+
+  void save(const std::string& path);
+
+  Image crop(int roq, int col, uint32_t rows, uint32_t cols);
+
+  Image scale(uint32_t rows, uint32_t cols,
+  Interpolation interp = ITERPOLATE_NEAREST);
+
+protected:
+  Image scale_nearest(uint32_t rows, uint32_t cols);
+  Image scale_bilinear(uint32_t rows, uint32_t cols);
+
+private:
+  uint8_t *_data;
+  uint32_t _rows;
+  uint32_t _cols;
+  uint8_t _bits_per_pixel;
+};
+
+} /* namespace */
+
+#endif /* _SEEGNIFY_IMAGE_H_ */
