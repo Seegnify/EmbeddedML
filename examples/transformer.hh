563e0283900312c9e74b554cbaab11a92370839b
GrootWoods <grootwoods@users.noreply.github.com>
2023-11-15 22:30:40 -0500

    Scaled Attention

--- examples/transformer.hh
+++ examples/transformer.hh
@@ -11,6 +11,47 @@
 
 using namespace seegnify;
 
+
+///////////////////////////////////
+// Attention
+///////////////////////////////////
+class Attention : public Function
+{
+public:
+  Attention(Graph& g, Function& q, Function& k, Function& v) :
+  Function(g), _k(k)
+  {
+    _k_dim = g.new_constant(1,1);
+
+    auto& qkT = *g.new_product(q, *g.new_transpose(_k));
+
+    auto& k_dim = *g.new_power(*g.new_broadcast(*_k_dim, qkT), -0.5);
+
+    auto& softmax = *g.new_softmax(*g.new_mul(qkT, k_dim));
+
+    _attention = g.new_product(softmax, v);
+
+    _attention->derivative(_graph.new_iderivative(*this));
+  }
+
+  virtual const Tensor& forward()
+  {
+    if (_value.size() > 0) return _value;
+
+    _k_dim->value() << _k.forward().cols();
+
+    _value = _attention->forward();
+
+    return _value;
+  }
+  
+protected:
+  Function& _k;
+  Constant* _k_dim;
+  Function* _attention;
+};
+
+
 ///////////////////////////////////
 // Transformer
 ///////////////////////////////////
@@ -27,13 +68,9 @@ public:
 
     return _value;
   }
-  
+
 protected:
 };
-
-///////////////////////////////////
-// Transformer
-///////////////////////////////////
 
 
 #endif /* _TRANSFORMER_MODEL_H_ */
